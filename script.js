becfe1312e0cd51c807c78da20c207ef785c67a1
pRostok <rostmart29@gmail.com>
2022-08-31 10:13:08 -0700

    navigation fix

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const start = document.getElementById('bat')
 const nav = document.getElementById('nav')
+const ul = document.getElementById('ul')
 
 start.addEventListener('click', () => {
     nav.classList.toggle('active')
+    if (nav.classList.contains('active')) {
+        setTimeout(() => ul.style.display = "flex", 400)
+    } else {
+        setTimeout(() => ul.style.display = "none", 500)
+    }
+    
+    
+    
 })
+
 
 const search = document.querySelector('.search')
 const btn = document.querySelector('.butt')
@@ -13,3 +23,4 @@ btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
+
